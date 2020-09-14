4e61b8693ab449ecaf0e60531ce43ffc7877facf
tacodes <64085493+tim-andes@users.noreply.github.com>
2020-09-14 09:03:27 -0400

    Converted into function

--- user_name_generator.py
+++ user_name_generator.py
@@ -1,13 +1,24 @@
-user = {}
+# this will need json file and unique id's for each user if used in user database
+def u_generator():
+	'''Generates dict and username from user provided first and last name.
+		Ex. first: tim, last: andes, username: tandes
 
-# input
-u_input_first = input("What is your first name? ").lower()
-u_input_last = input("What is your last name? ").lower()
+	Input: User input required, no function arguments.
+		Returns: Created username extracted from dict.'''
+	user = {}
+  
+	# Input: Get user's first/last name
+	u_input_first = input("What is your first name? ").lower()
+	u_input_last = input("What is your last name? ").lower()
+  
+	# create dict entries for first / last name
+	user["first"], user["last"] = u_input_first, u_input_last
+	# create username using first / last name key-value pairs
+	u_name = user['first'][0] + user['last']
+	# save 'username' as third and final dictionary entry
+	user["username"] = u_name
+  
+	# Output: Return and/or print
+	# print(f"\nSuggested username: {user['username']}")
+	return user["username"]
 
-# create
-user["first"], user["last"] = u_input_first, u_input_last
-u_name = user['first'][0] + user['last']
-user["username"] = u_name
-
-# output
-print(f"\nSuggested username: {user['username']}")
